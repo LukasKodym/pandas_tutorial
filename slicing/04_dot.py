e7b4e69439f8feeac26e8dda404ccf0827b68b0f
Lukas <lukasz.kodym@gmail.com>
2020-07-30 18:31:19 +0200

    slicing in progress

---
+++ slicing/04_dot.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# %%
+##
+
+import numpy as np
+import pandas as pd
+
+# %%
+##
+df = pd.DataFrame(np.random.randn(20, 5),
+                  columns=list('abcde'),
+                  index=list('abcdefghijklmnoprstu'))
+
+# %%
+##
+col_a = df.a
+
+mask = df.a > 0
+out = df[mask]
+
+out_v2 = df[df.a > 0]
+
+# %%
+##
+mask = (df.a > 0) & (df.c > 0)
+out = df[mask]
+
+out_v2 = df[(df.a > 0) & (df.c > 0)]
+
+# %%
+##
+mask = (df.a > 0) | (df.b < 0)
+out = df[mask]
